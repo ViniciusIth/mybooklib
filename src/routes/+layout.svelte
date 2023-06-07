5a9e89887ff35c00d04f21d552cc7f8b240a8022
ViniciusIth <vini.ithalo@hotmail.com>
2023-06-06 21:42:40 -0300

    feat: Added tab navigation ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,12 @@
 	<main>
 		<slot />
 	</main>
+
+	<nav class="tab-nav">
+		<a href="" class="tab-button"><span class="material-symbols-rounded">home</span></a>
+		<a href="" class="tab-button"><span class="material-symbols-rounded">explore</span></a>
+		<a href="" class="tab-button"><span class="material-symbols-rounded">more_horiz</span></a>
+	</nav>
 </div>
 
 <style>
@@ -15,5 +21,22 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
+	}
+
+	.tab-nav {
+		box-shadow: 0px -5px 6px 0px var(--color-bg-shade);
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		padding: 5px;
+	}
+
+	.tab-button {
+		border-radius: 15px;
+		padding: 10px 25px;
 	}
 </style>
